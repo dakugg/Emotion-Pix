7f24e211fe650a00ef68ade29c8d8dca3bd78972
dakugg <dakugabbarasinha72@gmail.com>
2025-04-12 08:56:56 +0530

    Update config.py

--- EmotionPix/config.py
+++ EmotionPix/config.py
@@ -1,9 +1,8 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv()  
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load from .env in local, ignored in Railway if using env vars
+
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-SECRET_KEY = os.getenv("SECRET_KEY")
-print("Supabase URL:", SUPABASE_URL)
-print("Supabase Key:", SUPABASE_KEY)
